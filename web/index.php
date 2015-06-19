0478a36752f551bd252ff7bf5edf852d2601c932
Merlyn Coslett <merlyn@cosle.tt>
2015-06-18 23:17:17 -0400

    removing unneeded routes

--- web/index.php
+++ web/index.php
@@ -36,21 +36,8 @@
     /***********
     ***ROUTES***
     ***********/
-    $app->get('/units/:unitCode', 'Skeleton\Controller\OperatingUnitController:getOperatingUnit');
-    $app->get('/units', 'Skeleton\Controller\OperatingUnitController:getOperatingUnits');
-
-    $app->get('/lots/:id', 'Skeleton\Controller\LotController:getLot');
-    $app->get('/lots', 'Skeleton\Controller\LotController:getLots');
-    $app->get('/lots/projects/:id', 'Skeleton\Controller\LotController:getLotsByProject');
-
-    $app->get('/models/:id', 'Skeleton\Controller\ModelController:getModel');
-    $app->get('/models', 'Skeleton\Controller\ModelController:getModels');
-
-    $app->get('/products/:id', 'Skeleton\Controller\ProductController:getProduct');
-    $app->get('/products', 'Skeleton\Controller\ProductController:getProducts');  
-    
-    $app->get('/projects/:id', 'Skeleton\Controller\ProjectController:getProject');
-    $app->get('/projects', 'Skeleton\Controller\ProjectController:getProjects');
+    $app->get('/skeleton/:id', 'Skeleton\Controller\SkeletonController:getSkeleton');
+    $app->get('/skeleton', 'Skeleton\Controller\SkeletonController:getSkeletons');
 
     $app->run();
 
